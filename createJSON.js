55766a8ed56d95959cda8b4d2f4442e2b9f9ad14
Rajan Savani <110261027+rajansavani@users.noreply.github.com>
2022-11-13 13:03:16 -0600

    create url for querying

---
+++ createJSON.js
@@ -0,0 +1,4 @@
+/* 
+ *   we will use the input from main to pull the appropriate data from Albion Data Project 
+ *   info will be stored as a JSON object that we can access later when performing calculations
+ */
--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 /*
  * let's start by filling in some info for the calc !
- * this is gonna be a lot but it will save a lot of time rather than calculating everything manually 
+ * this is gonna be a lot but it will save a lot of time rather than calculating everything manually
  */
-const material_type = "ore"; // what type of material are you refining (ore, wood, fiber, stone, hide)
+const material_type = "ORE"; // what type of material are you refining (ore, wood, fiber, stone, hide)
+const refined_type = "METALBAR";
 const buy_city = "Martlock"; // where you are sourcing the resources from
 const sell_city = "Lymhurst"; // where you are selling the refined product
-const return_rate = 0.367 // possible values include 0.152 / 0.435 in cities without bonus and 0.367 / 0.542 in cities with bonus
+const return_rate = 0.367; // possible values include 0.152 / 0.435 in cities without bonus and 0.367 / 0.542 in cities with bonus
 const using_focus = false;
-const sell_tax = 0.065 // with premium, 4% tax plus 2.5% setup fee
-const buy_fee = 0.025 // 2.5% setup fee
+const sell_tax = 0.065; // with premium, 4% tax plus 2.5% setup fee
+const buy_fee = 0.025; // 2.5% setup fee
 const quantity = 100; // how many of final product do we want to craft
 const usage_fee = 200; // usage fee of the crafting place
 /* input the mastery levels for each tier, will incorporate into calculation for focus refining */
@@ -19,38 +20,25 @@ const t7_mastery = 1;
 const t8_mastery = 1;
 const lowest_tier = 2; // the lowest tier refined resource we are going to buy
 const highest_tier = 5; // the tier we are trying to refine up to
-const quality = 1; // what rarity are we buying (ex. 4.1, 5.0, 6.3 etc)
+const quality = "1"; // what rarity are we buying (ex. 4.1, 5.0, 6.3 etc)
 /* this is optional, but it can be useful to record max load in order to determine how many resources you can carry in a single trip */
 const max_load = 3460; // in kg, not including any buffs from food
 const pie = true; // if true, means you are using pork pie to increase load
 const go_over = true; // if true, means you are willing to go up to 130% weig
-var store_data;
 
-/* this is all the information we need, now we can call the API to get current prices
-   if you need updated prices, please download the albion data project client and visit the markets! it helps everyone out
+/*
+ *	this is all the information we need, now we can call the API to get current prices
+ *	if you need updated prices, please download the albion data project client and visit the markets! it helps everyone out
  */
+var mat_string = "T" + lowest_tier + "_" + refined_type;
+for (var i = lowest_tier + 1; i <= highest_tier; i++) {
+  mat_string = mat_string + ",T" + i + "_" + material_type;
+}
+// outputs T2_METALBAR,T3_ORE,T4_ORE,T5_ORE,T5_METALBAR
+mat_string = mat_string + ",T" + highest_tier + "_" + refined_type; 
+// other inputs we need are the cities and qualities
+var city_string = buy_city + "," + sell_city;
+var quality_string = quality;
 
-const http = require('https');
-const { json } = require('stream/consumers');
-
-let req = http.get("https://www.albion-online-data.com/api/v2/stats/prices/T2_METALBAR,T3_ORE,T4_ORE,T5_ORE.json?locations=" + buy_city + "," + sell_city + "&qualities=" + quality + "&time-scale=24", function(res) {
-	let data = '',
-		store_data;
-
-	res.on('data', function(stream) {
-		data += stream;
-	});
-	res.on('end', function() {
-		store_data = JSON.parse(data);
-
-		// will output a Javascript object
-		console.log(store_data);
-	});
-});
-
-req.on('error', function(e) {
-    console.log(e.message);
-});
-
-/* gonna be testing with some hard-coding, will be updated later */
-console.log(store_data[0]);
+// this is all the info we need to create a link and grab the information
+var url = "https://www.albion-online-data.com/api/v2/stats/prices/" + mat_string + "?locations=" + city_string + "&qualities=" + quality_string + "&time-scale=24";
