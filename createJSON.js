bb9e14611be820abbb7c053d7362f90467346ad1
Rajan Savani <110261027+rajansavani@users.noreply.github.com>
2022-11-15 12:41:47 -0600

    created formatJSON.js
    formatJSON is used to remove unnecessary info for our calculations and make accessing data simpler

--- createJSON.js
+++
@@ -1,4 +0,0 @@
-/* 
- *   we will use the input from main to pull the appropriate data from Albion Data Project 
- *   info will be stored as a JSON object that we can access later when performing calculations
- */
---
+++ formatJSON.js
@@ -0,0 +1,30 @@
+/* 
+ * this script will take the .JSON file created from main.js and reformat it to make accessing data easier
+ */
+
+const original = require('./marketPrices.json');
+var formatted = {}; // empty JSON object that will be used to store formatted data
+const fs = require("fs"); // used to write JSON to disk
+
+original.forEach((item) => {
+    /* reduces each entry in array to just the sell price, buy price, and quality with city and item type acting as a key */
+    let primary_key = item.item_id + "_" + item.city;
+    let data = {
+        sell: item.sell_price_min,
+        buy: item.buy_price_max,
+        quality: item.quality
+    };
+    formatted[primary_key] = [];
+    formatted[primary_key].push(data);
+});
+
+const jsonString = JSON.stringify(formatted);
+/* write JSON to file */
+fs.writeFile("./formattedPrices.json", jsonString, "utf8", function (err) {
+  if (err) {
+    console.log("An error occured while writing JSON object to file");
+    return console.log(err);
+  } else {
+    console.log("JSON file has been saved");
+  }
+});
