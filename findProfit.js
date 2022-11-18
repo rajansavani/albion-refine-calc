f962c85dde090927919c28f29bbeff92990e745a
Rajan Savani <110261027+rajansavani@users.noreply.github.com>
2022-11-18 11:24:18 -0600

    created findProfit.js

---
+++ findProfit.js
@@ -0,0 +1,5 @@
+/*
+ * this script finds the most profitable items to refine including:
+ * the two types of materials, where to buy them, and where to sell them
+ * note that this script assumes you will refine in a city with a bonus and does not take focus into account
+ */
